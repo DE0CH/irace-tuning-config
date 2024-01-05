a46547fb55444c52e18306bb962028fde2fbf754
Deyao Chen <chendeyao000@gmail.com>
2024-01-05 15:56:30 +0800

    fix typo

--- jobs/cplex-and-minisat/run.sh
+++ jobs/cplex-and-minisat/run.sh
@@ -3,7 +3,7 @@
 # Run this script with cwd in the project root folder
 
 IRACE_TUNING_RUN_DIR=runs/cplex-and-minisat $(Rscript -e "cat(system.file(package='irace', 'bin', mustWork=TRUE))")/irace \
-    --train-instances-file jobs/cplex-and-minsat/instances.txt \
+    --train-instances-file jobs/cplex-and-minisat/instances.txt \
     --test-instances-dir Instances-test \
     --test-instances-file jobs/cplex-and-minisat/instances-test.txt \
     --target-runner ${IRACE_TUNING_TARGET_RUNNER:-../irace-tuning3/target-runner.py} \
