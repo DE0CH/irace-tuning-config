df3ecac8e63bcd7f7df6d1573c530a8c538cae58
Deyao Chen <chendeyao000@gmail.com>
2024-01-27 17:33:22 +0000

    add new surrogate

--- download.sh
+++ download.sh
@@ -5,5 +5,8 @@ curl -OLJ https://www.automl.org/wp-content/uploads/2019/10/minisat_randomk3.zip
 unzip minisat_randomk3.zip
 rm minisat_randomk3.zip
 curl -OLJ https://www.automl.org/wp-content/uploads/2019/10/cplex_rcw.zip
-unzip unzip cplex_rcw.zip
+unzip cplex_rcw.zip
 rm cplex_rcw.zip
+curl -OLJ https://www.automl.org/wp-content/uploads/2019/10/lingeling_circuitfuzz.zip
+unzip lingeling_circuitfuzz.zip
+rm lingeling_circuitfuzz.zip
