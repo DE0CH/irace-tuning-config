90e457c67bb5d7b62e20fed446031e4e7e271e74
Deyao Chen <chendeyao000@gmail.com>
2024-01-27 17:45:16 +0000

    add new surrogate

---
+++ jobs/lpg_zenotravel/gen.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+export IRACE_TUNING_PATH="${IRACE_TUNING_PATH:-../irace-tuning3}"
+
+"$IRACE_TUNING_PATH/tools/make_job_sh.py" \
+    --irace-tuning-run-dir '${IRACE_TUNING_RUN_DIR:-runs/lpg_zenotravel}' \
+    --train-instances-file jobs/lpg_zenotravel/instances.txt \
+    --test-instances-dir Instances-test \
+    --test-instances-file jobs/lpg_zenotravel/instances-test.txt \
+    --target-runner '${IRACE_TUNING_PATH}/target-runner.py' \
+    --log-file '${IRACE_TUNING_RUN_DIR}/train/irace.Rdata' \
+    --parameter-file jobs/parameters.txt \
+    --max-experiments 3000 --seed 123 \
+    --parallel '${IRACE_TUNING_NCPU:-1}' \
